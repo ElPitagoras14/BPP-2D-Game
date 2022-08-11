d0d7df432bf979be44b1b72e41f9cb941c0d061f
Jairo <jairoalc@espol.edu.ec>
2022-08-11 18:23:06 -0500

    Animales

--- scenes/Enciclopedia.gd
+++ scenes/Enciclopedia.gd
@@ -2,6 +2,7 @@ extends Control
 
 var indice = 0
 var list_data = []
+var vistaActual = "Arboles"
 onready var nombre = get_node("MarginContainer/VBoxContainer/HBoxContainer/CenterContainer/VBoxContainer2/Nombre")
 onready var nombreCientifico = get_node("MarginContainer/VBoxContainer/HBoxContainer/CenterContainer/VBoxContainer2/NombreCientifico")
 onready var descripcion = get_node("MarginContainer/VBoxContainer/HBoxContainer/MarginContainer/VBoxContainer/MarginContainer/RichTextLabel")
@@ -14,7 +15,13 @@ func _on_TextureButton_pressed():
 
 func load_data():
 	var file = File.new()
-	file.open("res://data/arboles_data.dat", file.READ)
+	list_data = []
+	indice = 0
+	
+	if vistaActual == "Arboles":
+		file.open("res://data/arboles_data.dat", file.READ)
+	else:
+		file.open("res://data/animales_data.dat", file.READ)
 	while !file.eof_reached():
 		var data = file.get_csv_line()
 		if data.size() > 3:
@@ -37,3 +44,13 @@ func mostrarInfo():
 	nombre.text = String(list_data[indice][0])
 	nombreCientifico.text = String(list_data[indice][1])
 	descripcion.text = String(list_data[indice][2])
+
+
+func _on_Arboles_pressed():
+	vistaActual = "Arboles"
+	load_data()
+
+
+func _on_Animales_pressed():
+	vistaActual = "Animales"
+	load_data()
