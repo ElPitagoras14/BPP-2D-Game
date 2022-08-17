9bfa632865aa6430d3d8bb2e5e9c2b7377ca66bc
josamaci <josamaci@espol.edu.ec>
2022-08-17 15:43:51 -0500

    Almanaque

--- scenes/Enciclopedia.gd
+++ scenes/Enciclopedia.gd
@@ -1,33 +1,45 @@
 extends Control
 
 var indice = 0
-var list_data = []
+var list_data_arboles = []
+var list_data_animales = []
+
 var vistaActual = "Arboles"
 onready var nombre = get_node("MarginContainer/VBoxContainer/HBoxContainer/CenterContainer/VBoxContainer2/Nombre")
 onready var nombreCientifico = get_node("MarginContainer/VBoxContainer/HBoxContainer/CenterContainer/VBoxContainer2/NombreCientifico")
 onready var descripcion = get_node("MarginContainer/VBoxContainer/HBoxContainer/MarginContainer/VBoxContainer/MarginContainer/RichTextLabel")
+onready var imagenArbol = get_node("MarginContainer/VBoxContainer/HBoxContainer/CenterContainer/VBoxContainer2/Arbol")
+onready var btnArboles = get_node("MarginContainer/VBoxContainer/HBoxContainer3/HBoxContainer/Arboles")
+onready var btnAnimales = get_node("MarginContainer/VBoxContainer/HBoxContainer3/HBoxContainer/Animales")
 
 func _ready():
 	load_data()
+	btnArboles.disabled = true
 
 func _on_TextureButton_pressed():
 	get_tree().change_scene("res://scenes/MainMenu.tscn")
 
 func load_data():
-	var file = File.new()
-	list_data = []
+	var fileArboles = File.new()
+	var fileAnimales = File.new()
 	indice = 0
+	fileArboles.open("res://data/arboles_data.dat", fileArboles.READ)
+	fileAnimales.open("res://data/animales_data.dat", fileAnimales.READ)
 	
-	if vistaActual == "Arboles":
-		file.open("res://data/arboles_data.dat", file.READ)
-	else:
-		file.open("res://data/animales_data.dat", file.READ)
-	while !file.eof_reached():
-		var data = file.get_csv_line()
+	while !fileArboles.eof_reached():
+		var data = fileArboles.get_csv_line()
 		if data.size() > 3:
-			list_data.append(data)
-	file.close()
-	list_data.remove(0)
+			list_data_arboles.append(data)
+			
+	while !fileAnimales.eof_reached():
+		var data = fileAnimales.get_csv_line()
+		if data.size() > 3:
+			list_data_animales.append(data)
+			
+	fileArboles.close()
+	fileAnimales.close()
+	list_data_animales.remove(0)
+	list_data_arboles.remove(0)
 	mostrarInfo()
 
 func _on_back_pressed():
@@ -36,21 +48,39 @@ func _on_back_pressed():
 		mostrarInfo()
 
 func _on_next_pressed():
-	if indice < list_data.size() - 1:
-		indice += 1
-		mostrarInfo()
+	if vistaActual == "Arboles":
+		if indice < list_data_arboles.size() - 1:
+			indice += 1
+			mostrarInfo()
+	else:
+		if indice < list_data_animales.size() - 1:
+			indice += 1
+			mostrarInfo()
 
 func mostrarInfo():
-	nombre.text = String(list_data[indice][0])
-	nombreCientifico.text = String(list_data[indice][1])
-	descripcion.text = String(list_data[indice][2])
+	if vistaActual == "Arboles":
+		var imagenPrincipal = load("res://assets/cards/"+str(list_data_arboles[indice][3])+".png")
+		nombre.text = String(list_data_arboles[indice][0])
+		nombreCientifico.text = String(list_data_arboles[indice][1])
+		descripcion.text = String(list_data_arboles[indice][2])
+		imagenArbol.texture = imagenPrincipal
+	else:
+		nombre.text = String(list_data_animales[indice][0])
+		nombreCientifico.text = String(list_data_animales[indice][1])
+		descripcion.text = String(list_data_animales[indice][2])
 
 
 func _on_Arboles_pressed():
 	vistaActual = "Arboles"
-	load_data()
+	btnArboles.disabled = true
+	btnAnimales.disabled = false
+	indice = 0
+	mostrarInfo()
 
 
 func _on_Animales_pressed():
 	vistaActual = "Animales"
-	load_data()
+	btnAnimales.disabled = true
+	btnArboles.disabled = false
+	indice = 0
+	mostrarInfo()
