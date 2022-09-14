3df1e6ac213639389a6ebb7addcbb7e894ba3dc8
josamaci <josamaci@espol.edu.ec>
2022-09-13 20:56:02 -0500

    cambios pantalla principa

--- scenes/PantallaInicio.gd
+++ scenes/PantallaInicio.gd
@@ -3,25 +3,36 @@ extends Control
 func _ready():
 	get_node("BGM").play()
 
-func _on_TextureButton_pressed():
-	get_node("Click").play()
-	$Popup.popup()
-
-
 func _on_Button2_pressed():
 	get_node("Click").play()
 	yield(get_tree().create_timer(.4),"timeout")
 	get_tree().quit()
 
-
 func _on_Button_pressed():
 	get_node("Click").play()
 	$Popup.hide()
 
-
 func _on_Nuevo_pressed():
-	pass # Replace with function body.
-
+	get_node("Click").play()
+	$Nombre.popup()
 
 func _on_Cargar_pressed():
-	pass # Replace with function body.
+	get_node("Click").play()
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
+
+func _on_Cerrar_pressed():
+	get_node("Click").play()
+	$Popup.popup()
+
+func _on_Cerrar2_pressed():
+	get_node("Click").play()
+	$Seleccionar.hide()
+	$Nombre.popup()
+
+func _on_Cerrar3_pressed():
+	get_node("Click").play()
+	$Nombre.hide()
+
+func _on_Continuar_pressed():
+	get_node("Click").play()
+	$Seleccionar.popup()
