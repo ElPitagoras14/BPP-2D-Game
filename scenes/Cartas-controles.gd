9ff8d58595285c45fe646c25129d172f383bb6de
Jairo <jairoalc@espol.edu.ec>
2022-09-14 19:16:58 -0500

    Pantalla de presentacion

--- scenes/Cartas-controles.gd
+++ scenes/Cartas-controles.gd
@@ -1,20 +1,9 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("BGM").play()
-	pass # Replace with function body.
 
 func _on_Cerrar_pressed():
 	get_node("Click").play()
 	yield(get_tree().create_timer(.3),"timeout")
 	get_tree().change_scene("res://scenes/Cartas-principal.tscn")
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- scenes/Cartas-principal.gd
+++ scenes/Cartas-principal.gd
@@ -1,37 +1,22 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("BGM").play()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _on_TextureButton4_pressed():
 	get_node("Click").play()
 	yield(get_tree().create_timer(.3),"timeout")
 	get_tree().change_scene("res://scenes/MainMenu.tscn")
-	pass # Replace with function body.
-
 
 func _on_TextureButton_pressed():
 	get_node("Click").play()
 	yield(get_tree().create_timer(.3),"timeout")
 	get_tree().change_scene("res://scenes/Cartas-controles.tscn")
-	pass # Replace with function body.
-
 
 func _on_JUGAR_pressed():
 	get_node("Click").play()
-	yield(get_tree().create_timer(.3),"timeout")
+	$Ayuda.popup()
+
+func _on_Cerrar_pressed():
+	$Ayuda.hide()
 	get_tree().change_scene("res://scenes/Cartas.tscn")
-	pass # Replace with function body.
