13944afb00ae860764416a017a1c636324d256a3
crianlop <66736610+crianlop@users.noreply.github.com>
2022-09-14 22:43:04 -0500

    cambio de nombres en sprites, implementacion de agregado de jugadores

--- scenes/PantallaInicio.gd
+++ scenes/PantallaInicio.gd
@@ -1,4 +1,5 @@
 extends Control
+var Nombre
 
 func _ready():
 	get_node("BGM").play()
@@ -36,4 +37,36 @@ func _on_Cerrar3_pressed():
 
 func _on_Continuar_pressed():
 	get_node("Click").play()
+	Nombre = $Nombre/TextEdit.text
 	$Seleccionar.popup()
+
+
+func _on_personaje1_pressed():
+	get_node("Click").play()
+	GameManager.addPlayer(Nombre, 1)
+	GameManager.loadPlayer(Nombre)
+	GameManager.saveJson(GameManager.allPlayers)
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
+
+
+
+func _on_personaje2_pressed():
+	get_node("Click").play()
+	GameManager.addPlayer(Nombre, 2)
+	GameManager.loadPlayer(Nombre)
+	GameManager.saveJson(GameManager.allPlayers)
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
+
+func _on_personaje3_pressed():
+	get_node("Click").play()
+	GameManager.addPlayer(Nombre, 3)
+	GameManager.loadPlayer(Nombre)
+	GameManager.saveJson(GameManager.allPlayers)
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
+
+func _on_personaje4_pressed():
+	get_node("Click").play()
+	GameManager.addPlayer(Nombre, 4)
+	GameManager.loadPlayer(Nombre)
+	GameManager.saveJson(GameManager.allPlayers)
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
